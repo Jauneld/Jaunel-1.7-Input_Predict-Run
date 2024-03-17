d74d313350c2f8343dd87409912ef7952fc9d176
Jdeans@2025 <144694485+Jauneld@users.noreply.github.com>
2024-03-17 01:27:05 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+#Jaunel Deans 
+#September 18, 2023
+
+#Task 1 - Add comments to the code below to predict what will happen when it is run.
+
+#Task 2 - add comments to the code to explain what the variable(s) are called and where they are used. Make sure to show where the variable.
+
+print("Hi! What's your name?")
+
+name = input()
+
+print("Hi " + name + "! How are you today?")
+# I think that the program will print the statement, "Hi! What's your name?". The user will type their answer. The input the user typed will be displayed in the place of the variable name because input() was assigned to name. 
+name = input("What is your name?")
